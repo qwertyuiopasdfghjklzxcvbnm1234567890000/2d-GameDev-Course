5c4ffd797223b0ac6499dc35b65b51110ee5f4f8
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2024-03-18 14:14:42 -0700

    l4 the boosting ship

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -2,7 +2,8 @@ extends Sprite2D
 
 var max_speed := 600.0
 var velocity := Vector2(0,0)
-
+var boost_speed := 1500.0
+var normal_speed := 600.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
@@ -10,9 +11,15 @@ func _process(delta: float) -> void:
 	direction.y = Input.get_axis("move_up","move_down")
 	if direction.length() > 1.0:
 		direction = direction.normalized()
-		
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
 	velocity = direction * max_speed
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
 	
+
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
