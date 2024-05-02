d2ff35ee16c9c35944606a49246c1849f48bb86d
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2024-05-02 13:13:39 -0700

    L6 lesson complete; L6 practice incomplete

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var canvas_group: CanvasGroup = $CanvasGroup
-
+@export var possible_items: Array[PackedScene] = []
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -32,3 +32,17 @@ func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
 func open() -> void:
 	animation_player.play("open")
 	input_pickable = false
+	if possible_items.is_empty():
+		return
+	for current_index in range(randi_range(1,3)):
+		_spawn_random_items()
+	
+func _spawn_random_items() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	add_child(loot_item)
+	var random_angle := randf_range(0.0, 2.0 * PI)
+	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
+	var random_distance := randf_range(60.0, 120.0)
+	loot_item.position = random_direction * random_distance
+	
+
